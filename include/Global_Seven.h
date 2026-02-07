c1082931b0cc41c437dbb2330ff578f3b331e176
Don Michael Feeney Jr. <dfeen87@gmail.com>
2026-02-07 17:54:20 -0500

    Include algorithm for clamp in telemetry

--- include/Global_Seven.h
+++ include/Global_Seven.h
@@ -441,8 +441,6 @@ public:
 
 private:
     std::shared_ptr<ETHState> state_;
-    struct ETHState;
-    static std::shared_ptr<ETHState> state_;
 };
 
 class PolygonAdapter final : public IChainAdapter {
