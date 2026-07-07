15d1af7607689d6e9e600359781bdc2409b04497
Don Michael Feeney Jr. <dfeen87@gmail.com>
2026-07-07 15:37:43 -0400

    Create ReplayEngine.h

---
+++ include/l4/ReplayEngine.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "l4/ReplayBuffer.h"
+#include <string>
+
+namespace ailee {
+namespace l4 {
+
+struct ReplayEngine {
+    ReplayBuffer buffer;
+
+    void load_replay_file(const std::string& path);
+    void write_replay_file(const std::string& path, const ReplayBuffer& replay_buffer) const;
+
+    bool replay_tick(size_t tick_index,
+                     DeterministicSchedulerState& out_scheduler,
+                     ClusterView& out_view,
+                     TelemetrySample& out_telemetry) const;
+
+    bool verify_tick(size_t tick_index,
+                     const DeterministicSchedulerState& scheduler,
+                     const ClusterView& view,
+                     const TelemetrySample& telemetry) const;
+};
+
+} // namespace l4
+} // namespace ailee
