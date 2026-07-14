031fc901381fe1df26055851317db67b93552a50
Don Michael Feeney Jr. <dfeen87@gmail.com>
2026-07-14 10:31:38 -0400

    Create ConsensusRules.cpp

---
+++ src/protocol/ConsensusRules.cpp
@@ -0,0 +1,8 @@
+// ConsensusRules.cpp
+// Temporary stub to satisfy CMake until full implementation is added.
+
+#include "ConsensusRules.hpp"
+
+bool ConsensusRules::validate() const {
+    return true; // placeholder
+}
