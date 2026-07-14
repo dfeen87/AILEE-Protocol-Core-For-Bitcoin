cb03acc3fc6ce2a0d050b927511bf86c2144d40b
Don Michael Feeney Jr. <dfeen87@gmail.com>
2026-07-13 23:37:53 -0400

    Create SwarmHttpServer.cpp

---
+++ src/orchestration/SwarmHttpServer.cpp
@@ -0,0 +1,7 @@
+#include "SwarmHttpServer.h"
+#include "ProverSwarm.h"
+
+void SwarmHttpServer::attach(ProverSwarm& /*swarm*/) {
+    // TODO: integrate with AILEEWebServer to expose /prover/register, etc.
+    // For now, this is a stub so the build succeeds.
+}
